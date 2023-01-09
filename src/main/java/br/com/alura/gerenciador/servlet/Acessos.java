3bde4f640589a8c06ed14340a128c8e729c55ef6
renanbevi <renanbevi@gmail.com>
2023-01-09 16:02:14 -0300

    Criei uma classe acesso para listar todos os usuários e senhas que criamos na classe banco como um LIST

---
+++ src/main/java/br/com/alura/gerenciador/servlet/Acessos.java
@@ -0,0 +1,33 @@
+package br.com.alura.gerenciador.servlet;
+
+public class Acessos {
+
+	private Integer id;
+	private String admin;
+	private String senha;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(String admin) {
+		this.admin = admin;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
