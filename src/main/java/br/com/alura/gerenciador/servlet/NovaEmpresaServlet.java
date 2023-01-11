bfd1249c7d4752121afed6271cf676d76343c485
renanbevi <renanbevi@gmail.com>
2023-01-11 18:47:19 -0300

    Inclui mais um campo data para informar a data de abertura da empresa

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,7 +1,9 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -24,11 +26,22 @@ public class NovaEmpresaServlet extends HttpServlet {
 	
 		String nomeEmpresa = request.getParameter("empresa");
 		String cnpjEmpresa = request.getParameter("cnpj");
+		String paramDataEmpresa = request.getParameter("data"); // recebe a informação da data do formulario
 	
-			
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); // sdf formata o parse da string DATA
+			dataAbertura = sdf.parse(paramDataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
+		
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
 		empresa.setCnpj(cnpjEmpresa);
+		empresa.setDataAbertura(dataAbertura);
+		
 		
 		
 		Banco banco = new Banco();
@@ -40,6 +53,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");  //usar o despachador de requisição
 		request.setAttribute("empresa", empresa.getNome()); //Joga o atributo dentro da requisição 
 		request.setAttribute("cnpj", empresa.getCnpj());
+		request.setAttribute("data", empresa.getDataAbertura());
 		rd.forward(request, response); //leva os objetos e chama o JSP
 		
 		
